e54c1cea81a406183e16dd5ea13a91425b5e851e
hariharanr261 <hariharanr261@gmail.com>
2024-12-18 08:57:02 +0530

    first commit

---
+++ src/Tasks/Pattern_program.java
@@ -0,0 +1,19 @@
+package Tasks;
+
+public class Pattern_program
+{
+
+	public static void main(String[] args) 
+	{
+		for(int a = 1;a<=5;a++)
+		{
+			
+			  for(int b=1;b<=a;b++) 
+		   
+			  System.out.print("*");
+		  
+              System.out.println();
+	    }
+	}
+
+}
